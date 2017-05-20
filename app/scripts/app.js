aa2f8c456921996399f9fcaa254b6e2c1673b659
Jorge Arias <mail@jorgearias.cl>
2017-05-20 00:11:53 -0400

    Initial commit. Created Yeoman bootstrap project

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dssiFrontApp
+ * @description
+ * # dssiFrontApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dssiFrontApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
