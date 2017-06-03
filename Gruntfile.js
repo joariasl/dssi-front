3681782ba7f97559d8293701e81761b898f2e619
Jorge Arias <mail@jorgearias.cl>
2017-06-03 14:23:40 -0400

    Added static JavaScript files
    * Grunt reconfigured
    * Moved Gentella JavaScript template

--- Gruntfile.js
+++ Gruntfile.js
@@ -413,7 +413,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'js/*.js'
           ]
         }, {
           expand: true,
