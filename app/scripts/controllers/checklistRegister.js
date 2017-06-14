8aaa72a0920a8de021a247e611514498f4498754
Ignacio Miranda <imiranda4305@gmail.com>
2017-06-13 21:41:44 -0400

    Checklist-register renderer according to model
    Added renderer checklist-register and modify spaces in checklist-view.

--- app/scripts/controllers/checklistRegister.js
+++ app/scripts/controllers/checklistRegister.js
@@ -9,4 +9,58 @@
  */
 angular.module('dssiFrontApp')
   .controller('ChecklistRegisterCtrl', function () {
+    var vm = this;
+    vm.checklistTurns= [
+      {
+        turn: 'Mañana'
+      },
+      {
+        turn: 'Tarde'
+      },
+      {
+        turn: 'Noche'
+      }
+    ];
+    vm.checklistItemGroups = [
+      {
+        name: 'Operatividad',
+        items: [
+          {
+            name: 'Equipo radial'
+          },
+          {
+            name: 'Computador'
+          },
+          {
+            name: 'Sistemas de cámaras'
+          },
+          {
+            name: 'Sistema de enrolamiento'
+          },
+          {
+            name: 'Celular'
+          },
+          {
+            name: 'Pantalla eventos actualizada'
+          },
+          {
+            name: 'Ascensores'
+          }
+        ],
+      },
+      {
+        name: 'Actividad',
+        items: [
+          {
+            name: 'Retiro de basura'
+          },
+          {
+            name: 'Ingreso camión de valores'
+          },
+          {
+            name: 'Retiro de lavandería'
+          }
+        ],
+      }
+    ];
   });
