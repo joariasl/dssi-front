a50c4fa9b6d3294e5824f7f4e920c255c799a87a
Jorge Arias <mail@jorgearias.cl>
2017-06-27 04:37:00 -0400

    Functional Property selector

--- app/scripts/controllers/property.js
+++ app/scripts/controllers/property.js
@@ -8,29 +8,19 @@
  * Controller of the dssiFrontApp
  */
 angular.module('dssiFrontApp')
-  .controller('PropertyCtrl', function (Property) {
+  .controller('PropertyCtrl', function (Property, $localStorage, $filter, $rootScope) {
     var vm = this;
-    vm.property_selected;
-    // vm.property = [
-    //   {
-    //     id: 1,
-    //     name: 'Casino 1',
-    //   },
-    //   {
-    //     id: 2,
-    //     name: 'Casino 2',
-    //   },
-    //   {
-    //     id: 3,
-    //     name: 'Casino 3',
-    //   }
-    // ];
-    vm.property = Property.query(function(data){
+    vm.change = change;
+    vm.propertyId = $localStorage.propertyId;
+    vm.properties = Property.query(function(data){
       if(data.length > 0){
-        vm.property_selected = data[0];
+        vm.property = $filter('filter')(data, {'id':vm.propertyId}, true)[0];
+        $rootScope.property = vm.property;
       }
     });
     ////////////
-    function change(){
+    function change(property){
+      vm.property = property;
+      $localStorage.propertyId = vm.property.id;
     }
   });
