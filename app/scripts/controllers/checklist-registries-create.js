12788fbc7c6e215e1a8674cfe922a6927b7fd4f4
Jorge Arias <mail@jorgearias.cl>
2017-06-30 03:56:07 -0400

    Added datepicker with angular-ui and Moment.js

--- app/scripts/controllers/checklist-registries-create.js
+++ app/scripts/controllers/checklist-registries-create.js
@@ -8,9 +8,27 @@
  * Controller of the dssiFrontApp
  */
 angular.module('dssiFrontApp')
-  .controller('ChecklistRegistriesCreateCtrl', function (Turns) {
+  .controller('ChecklistRegistriesCreateCtrl', function (Turns, moment) {
     var vm = this;
     vm.checklistTurns = Turns.turns;
+
+    // Datepicker
+    vm.datepicker = {
+      format: 'dd-MM-yyyy',
+      date: new Date(),
+      opened: false,
+      dateOptions: {
+        formatYear: 'yy',
+        maxDate: moment(),
+        minDate: moment().subtract(1, 'month'),
+        startingDay: 1
+      },
+      altInputFormats: ['d!/M!/yyyy'],
+      open: function(){
+        vm.datepicker.opened = true;
+      }
+    };
+
     vm.checklistItemGroups = [
       {
         name: 'Operatividad',
