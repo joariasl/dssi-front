339095d9dba07e4781b4feb2d2a02f1fd801b5db
Ignacio Miranda <imiranda4305@gmail.com>
2017-07-02 02:29:42 -0400

    Restructured controllers and views for KeyLoans.

---
+++ app/scripts/controllers/key-loans-delivery.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dssiFrontApp.controller:KeyLoansDeliveryCtrl
+ * @description
+ * # KeyLoansDeliveryCtrl
+ * Controller of the dssiFrontApp
+ */
+angular.module('dssiFrontApp')
+  .controller('KeyLoansDeliveryCtrl', function () {
+  });
