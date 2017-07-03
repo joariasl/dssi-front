6a1aefd9bcaee96d3f8da88a779f7f18b94e3301
Ignacio Miranda <imiranda4305@gmail.com>
2017-07-03 02:12:28 -0400

    Form updated to key loans delivery.

--- app/scripts/controllers/key-loans-delivery.js
+++ app/scripts/controllers/key-loans-delivery.js
@@ -8,5 +8,25 @@
  * Controller of the dssiFrontApp
  */
 angular.module('dssiFrontApp')
-  .controller('KeyLoansDeliveryCtrl', function () {
+  .controller('KeyLoansDeliveryCtrl', function (moment, $localStorage) {
+    var vm = this;
+    //Hour
+    vm.mytime = new Date();
+    // Datepicker
+    vm.datepicker = {
+      format: 'dd-MM-yyyy',
+      date: new Date(),
+      opened: false,
+      dateOptions: {
+        formatYear: 'yy',
+        maxDate: moment(),
+        minDate: moment().subtract(1, 'month'),
+        startingDay: 1
+      },
+      altInputFormats: ['d!/M!/yyyy'],
+      open: function(){
+        vm.datepicker.opened = true;
+      }
+    };
+
   });
