7ee13c848a36f44ffb7b27ce786942bf4bbe45b3
Jorge Arias <mail@jorgearias.cl>
2017-07-05 02:05:35 -0400

    Created ChecklistRegistry view based on create view

--- app/scripts/controllers/checklist-registry-view.js
+++ app/scripts/controllers/checklist-registry-view.js
@@ -8,10 +8,13 @@
  * Controller of the dssiFrontApp
  */
 angular.module('dssiFrontApp')
-  .controller('ChecklistRegistryViewCtrl', function ($state, $stateParams, ChecklistRegistry, ChecklistItemGroup, $localStorage, Turns) {
+  .controller('ChecklistRegistryViewCtrl', function ($state, $stateParams, ChecklistRegistry, ChecklistItemGroup, $localStorage, Turns, $filter) {
     var vm = this;
     vm.checklistTurns = Turns.turns;
-    vm.checklistsRegistry = ChecklistRegistry.get({
+    vm.checklistRegistry = ChecklistRegistry.get({
       id: $stateParams.id
+    });
+    vm.checklistRegistry.$promise.then(function(){
+      vm.total_ok = $filter('filter')(vm.checklistRegistry.checklist_entries, {response:1}).length;
     });
   });
