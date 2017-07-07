dcd76039b53b5c11187991cd411f3f8e809175c2
Ignacio Miranda <imiranda4305@gmail.com>
2017-07-06 21:35:32 -0400

    Updated KeyLoansDelivery view and controller

--- app/scripts/controllers/key-loans-delivery.js
+++ app/scripts/controllers/key-loans-delivery.js
@@ -8,13 +8,18 @@
  * Controller of the dssiFrontApp
  */
 angular.module('dssiFrontApp')
-  .controller('KeyLoansDeliveryCtrl', function (moment, $localStorage) {
+  .controller('KeyLoansDeliveryCtrl', function (moment, $localStorage, KeyLoan) {
     var vm = this;
+
+    vm.keyLoans = KeyLoan.query({
+      property_id: $localStorage.property_id
+    });
+
     //Hour
-    vm.mytime = new Date();
+    vm.mytime = new moment();
     // Datepicker
     vm.datepicker = {
-      format: 'dd-MM-yyyy',
+      format: 'dd/MM/yyyy',
       date: new Date(),
       opened: false,
       dateOptions: {
@@ -23,7 +28,7 @@ angular.module('dssiFrontApp')
         minDate: moment().subtract(1, 'month'),
         startingDay: 1
       },
-      altInputFormats: ['d!/M!/yyyy'],
+      altInputFormats: ['d!-M!-yyyy'],
       open: function(){
         vm.datepicker.opened = true;
       }
