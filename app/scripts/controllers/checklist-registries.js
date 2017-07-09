31a7e5be041202f04d4064961a7295efa0a9516f
Jorge Arias <mail@jorgearias.cl>
2017-07-09 02:16:40 -0400

    ChecklistRegistries list with paginate for smart-table

--- app/scripts/controllers/checklist-registries.js
+++ app/scripts/controllers/checklist-registries.js
@@ -8,17 +8,26 @@
  * Controller of the dssiFrontApp
  */
 angular.module('dssiFrontApp')
-  .controller('ChecklistRegistriesCtrl', function (ChecklistRegistry, ChecklistItemGroup, $localStorage, Turns, $filter, $log) {
+  .controller('ChecklistRegistriesCtrl', function (ChecklistRegistry, ChecklistItemGroup, $localStorage, Turns, $filter, $scope, $log) {
     var vm = this;
+    vm.load = load;
+
     vm.checklistTurns = Turns.turns;
-    vm.checklistItemGroups = ChecklistItemGroup.query({
-      property_id: $localStorage.property_id
-    });
-    vm.checklistItemGroups.$promise.then(function(){
+
+    ////////////
+
+    function loadChecklistRegistries(tableState){
+      $log.log(tableState);
+      var pagination = tableState.pagination;
+      var start = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
+      var number = pagination.number || 10;  // Number of entries showed per page.
+
       vm.checklistsRegistries = ChecklistRegistry.query({
-        property_id: $localStorage.property_id
-      });
-      vm.checklistsRegistries.$promise.then(function(){
+        property_id: $localStorage.property_id,
+        page: 1 + Math.floor(start/number)
+      }, function(result, headers){
+        // tableState.pagination.numberOfPages = 1 + Math.floor(vm.checklistsRegistries.length/number);
+        tableState.pagination.numberOfPages = 1 + Math.floor(headers('total')/headers('per_page'));
         angular.forEach(vm.checklistsRegistries, function(checklistRegistry, key) {
           checklistRegistry.total_ok = $filter('filter')(checklistRegistry.checklist_entries, {response:1}).length;
           checklistRegistry.total_groups = {};
@@ -34,5 +43,18 @@ angular.module('dssiFrontApp')
           });
         });
       });
-    });
+    }
+
+    function load(tableState){
+      if(!vm.checklistItemGroups || !vm.checklistItemGroups.$promise.$$state.status){
+        vm.checklistItemGroups = ChecklistItemGroup.query({
+          property_id: $localStorage.property_id
+        });
+        vm.checklistItemGroups.$promise.then(function(){
+          loadChecklistRegistries(tableState);
+        });
+      }else{
+        loadChecklistRegistries(tableState);
+      }
+    }
   });
