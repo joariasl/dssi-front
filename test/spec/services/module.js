745f5cbd31bdba5dba1771552c4f4e6f446cc723
Jorge Arias <mail@jorgearias.cl>
2017-07-13 01:35:26 -0400

    Menu rendered based in Module service

---
+++ test/spec/services/module.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Service: Module', function () {
+
+  // load the service's module
+  beforeEach(module('dssiFrontApp'));
+
+  // instantiate service
+  var Module;
+  beforeEach(inject(function (_Module_) {
+    Module = _Module_;
+  }));
+
+});
