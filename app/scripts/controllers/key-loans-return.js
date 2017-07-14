f028c3600e65a8a18780c587ab6c328671101fc3
Ignacio Miranda <imiranda4305@gmail.com>
2017-07-14 17:54:41 -0400

    Updated the view and controller in key-loan-return

--- app/scripts/controllers/key-loans-return.js
+++ app/scripts/controllers/key-loans-return.js
@@ -8,20 +8,64 @@
  * Controller of the dssiFrontApp
  */
 angular.module('dssiFrontApp')
-  .controller('KeyLoansReturnCtrl', function () {
+  .controller('KeyLoansReturnCtrl', function (moment, $localStorage, Key, KeyLoan, Amphitryon, notificationService, $log) {
     var vm = this;
-    vm.condition= [
-      {
-        name: 'Operativa'
+    vm.updateKeyConditionItem = updateKeyConditionItem;
+    vm.searchAmphitryon = searchAmphitryon;
+    vm.searchKey = searchKey;
+
+    // Datepicker
+    vm.datepicker = {
+      format: 'dd/MM/yyyy',
+      date: new Date(),
+      opened: false,
+      dateOptions: {
+        formatYear: 'yy',
+        maxDate: moment(),
+        minDate: moment().subtract(1, 'month'),
+        startingDay: 1
       },
-      {
-        name: 'Quebrada'
-      },
-      {
-        name: 'Dañada'
-      },
-      {
-        name: 'Otros'
+      altInputFormats: ['d!-M!-yyyy'],
+      open: function(){
+        vm.datepicker.opened = true;
       }
-    ];
+    };
+
+    vm.search_amphitryon_rut = null;
+    vm.amphitryon = null;
+    vm.search_key_code = null;
+    vm.key = null;
+
+    vm.keyLoan = new KeyLoan({
+      return_datetime: new Date()
+    });
+
+    ////////////
+
+    function updateKeyConditionItem(keyConditionItem){
+      delete keyConditionItem.key_condition;
+      KeyConditionItem.update({id: keyConditionItem.id}, keyConditionItem).$promise.then(function(){
+        notificationService.success('¡Actualizado!');
+      }, function(){
+        notificationService.error('No ha sido posible atender la solicitud.');
+      });
+    }
+
+    function searchAmphitryon(){
+      var rut = vm.search_amphitryon_rut;
+      var searchRut = rut.substr(0,rut.length-1).replace(/[^\d]*/g,'');
+      if(searchRut){
+        vm.amphitryon = Amphitryon.get({
+          person_rut: searchRut
+        });
+      }
+    }
+
+    function searchKey(){
+      var searchCode = vm.search_key_code;
+      vm.key = Key.get({
+        code: searchCode
+      });
+    }
+
   });
